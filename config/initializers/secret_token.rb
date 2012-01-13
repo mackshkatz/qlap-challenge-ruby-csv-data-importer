ee731d52c27a0b8ac48d846c5bb0ccfb33fd36a0
Max Katz <maxwelljkatz@gmail.com>
2012-01-13 10:22:20 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QlapChallengeRubyCsvDataImporter::Application.config.secret_token = '3a12d57ab977d7e15157ce00859e08d780a911ea38ecdc7dad0b06c16774bd8c4761e275ae9437c974d74f7abf5e5692cbdca6e6826faa883084fb2318433e9a'
